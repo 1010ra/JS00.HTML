6cb15e01c79b647e5440cea301b92ba170cdcde1
1010ra <124473202+1010ra@users.noreply.github.com>
2023-03-06 13:20:14 +0100

    Create rakia.js

---
+++ rakia.js
@@ -0,0 +1,10 @@
+let perssonne ={
+    "nom" :'ramla',
+    "age" : '20',
+"village" :'gardjé',
+
+};
+
+console.log("nom",perssonne.nom);
+console.log ("age",perssonne.age);
+console.log("village",perssonne.village);
