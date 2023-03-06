e5803dfd7ddecc7f35836588dca7f4f24b4d12e9
1010ra <124473202+1010ra@users.noreply.github.com>
2023-03-06 13:13:49 +0100

    Create adamou.js

---
+++ adamou.js
@@ -0,0 +1,10 @@
+let personne ={
+    "nom" :'rakia',
+"age" :'25',
+"ville":'niamey',
+
+};
+
+console.log('nom',personne.nom);
+console.log('age',personne.age);
+console.log('ville',personne.ville);
